c932117a72ae5bfec0806245c195e5a8abc69315
Clément DOUIN <soywod@users.noreply.github.com>
2019-12-16 13:01:42 +0100

    move cypress webpack config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,29 @@
+const path = require("path")
 const webpack = require("@cypress/webpack-preprocessor")
+
+const webpackOptions = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+}
 
 module.exports = on => {
   const options = {
-    webpackOptions: require("../webpack.config"),
+    webpackOptions,
     watchOptions: {},
   }
 
