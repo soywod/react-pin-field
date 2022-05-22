71b4e45b532c255253ff267159e30887a435ae4a
Clément DOUIN <clement.douin@posteo.net>
2022-05-22 18:15:48 +0200

    update license, add size limit

---
+++ .size-limit.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    path: "dist/react-pin-field.cjs.production.min.js",
+    limit: "10 KB",
+  },
+  {
+    path: "dist/react-pin-field.esm.js",
+    limit: "10 KB",
+  },
+];
