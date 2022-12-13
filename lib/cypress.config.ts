03932542f27cc08f6d4633b3cf2e60c73f1aa636
Clément DOUIN <clement.douin@posteo.net>
2022-12-13 21:44:13 +0100

    upgrade packages, prepare v3.1.0

---
+++ lib/cypress.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from "cypress";
+
+export default defineConfig({
+  fixturesFolder: false,
+  video: false,
+  e2e: {
+    setupNodeEvents(on) {
+      on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "src/**/*.e2e.ts",
+    supportFile: false,
+  },
+});
--- lib/src/utils/utils.ts
+++ lib/src/utils/utils.ts
@@ -6,7 +6,7 @@ export function range(start: number, length: number): number[] {
   return Array.from({length}, (_, i) => i + start);
 }
 
-export function omit<T = Record<string, unknown>>(keys: string[], input: T): T {
+export function omit<T extends Record<string, unknown>>(keys: string[], input: T): T {
   let output: T = Object.create({});
 
   for (let key in input) {
