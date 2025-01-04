c8aa6be9442a459ec6f093ab3d4537c1afdba5d9
Clément DOUIN <clement.douin@posteo.net>
2025-01-04 13:50:08 +0100

    support strict mode and controlled mode

--- src/pin-field/pin-field-v2.stories.tsx
+++ src/pin-field/pin-field-v2.stories.tsx
@@ -1,7 +1,8 @@
+import { StrictMode as ReactStrictMode } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
-import { PinFieldV2, defaultProps, Props } from "./pin-field-v2";
+import { PinFieldV2, defaultProps, Props, usePinField } from "./pin-field-v2";
 
 const defaultArgs = {
   length: defaultProps.length,
@@ -27,6 +28,35 @@ const meta: Meta<typeof PinFieldV2> = {
 
 export const Default: StoryObj<typeof PinFieldV2> = {
   render: props => <PinFieldV2 {...props} data-cy="pin-field" />,
+  args: defaultArgs,
+};
+
+export const StrictMode: StoryObj<typeof PinFieldV2> = {
+  render: props => (
+    <ReactStrictMode>
+      <PinFieldV2 {...props} />
+    </ReactStrictMode>
+  ),
+  args: defaultArgs,
+};
+
+export const Controlled: StoryObj<typeof PinFieldV2> = {
+  render: props => {
+    const handler = usePinField(props.length);
+
+    return (
+      <>
+        <PinFieldV2 {...props} handler={handler} />
+        <button onClick={() => handler.refs.current[0]?.focus()}>focus first</button>
+        <input
+          type="text"
+          placeholder="custom pin"
+          value={handler.value}
+          onChange={event => handler.setValue(event.target.value)}
+        />
+      </>
+    );
+  },
   args: defaultArgs,
 };
 
--- src/pin-field/pin-field-v2.tsx
+++ src/pin-field/pin-field-v2.tsx
@@ -1,6 +1,24 @@
-import { FC, useEffect, useReducer, useRef } from "react";
+import {
+  FC,
+  useEffect,
+  useReducer,
+  useRef,
+  KeyboardEventHandler,
+  KeyboardEvent,
+  ChangeEventHandler,
+  useCallback,
+  CompositionEventHandler,
+  forwardRef,
+  useImperativeHandle,
+  RefObject,
+  ActionDispatch,
+  useMemo,
+} from "react";
 
 import { noop, range } from "../utils";
+
+const BACKSPACE = 8;
+const DELETE = 46;
 
 export type DefaultProps = {
   length: number;
@@ -24,77 +42,262 @@ export const defaultProps: DefaultProps = {
   onComplete: noop,
 };
 
-export type Props = Partial<DefaultProps>;
+export type Props = Partial<DefaultProps> & {
+  handler?: Handler;
+};
 
 export type State = {
   cursor: number;
   values: string[];
   length: number;
+  backspace: boolean;
+  composition: boolean;
 };
 
 export function defaultState(length: number): State {
   return {
     cursor: 0,
-    values: [],
+    values: Array(length),
     length,
+    backspace: false,
+    composition: false,
   };
 }
 
-export type Action = { type: "handle-change"; key: number; value: string };
+export type Action =
+  | { type: "handle-change"; index: number; value: string | null; reset?: boolean }
+  | { type: "handle-key-down"; index: number; event: KeyboardEvent<HTMLInputElement> }
+  | { type: "start-composition"; index: number }
+  | { type: "end-composition"; index: number; value: string };
 
 export function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case "handle-change": {
-      console.log("action", action);
-      state.values[action.key] = action.value;
+  console.log("action", action);
 
-      if (action.value === "") {
-        state.cursor = action.key - 1;
+  switch (action.type) {
+    case "start-composition": {
+      return { ...state, composition: true };
+    }
+
+    case "end-composition": {
+      state.composition = false;
+
+      if (action.value) {
+        state.values[action.index] = action.value;
       } else {
-        state.cursor = action.key + 1;
+        delete state.values[action.index];
       }
+
+      const dir = state.values[action.index] ? 1 : 0;
+      state.cursor = Math.min(action.index + dir, state.length - 1);
+
+      return { ...state };
+    }
+
+    case "handle-change": {
+      if (state.composition) {
+        break;
+      }
+
+      if (action.reset) {
+        state.values = Array(state.length);
+      }
+
+      if (action.value) {
+        const values = action.value.split("");
+        const length = Math.min(state.length - action.index, values.length);
+        state.values.splice(action.index, length, ...values.slice(0, length));
+        state.cursor = Math.min(action.index + length, state.length - 1);
+      } else {
+        delete state.values[action.index];
+        const dir = state.backspace ? 0 : 1;
+        state.cursor = Math.max(0, action.index - dir);
+      }
+
+      return { ...state, backspace: false };
+    }
+
+    case "handle-key-down": {
+      const key = action.event.key === "Backspace" || action.event.key === "Delete";
+      const which = action.event.which === BACKSPACE || action.event.which === DELETE;
+      const keyCode = action.event.keyCode === BACKSPACE || action.event.keyCode === DELETE;
+      const deletion = key || which || keyCode;
+
+      // Deletion is a bit tricky and requires special attention.
+      //
+      // When the current field has a value, deletion works as
+      // expected AS LONG AS THE STATE IS NOT UPDATED and keeps its
+      // reference: the value deletes by itself and the `onchange`
+      // event is triggered, which updates the state.
+      //
+      // But when the current field is empty, deletion does not
+      // trigger the `onchange` event. Therefore the state needs to be
+      // updated here. Moving the cursor backwards is enough for
+      // deletion to happen on the previous field, which triggers the
+      // `onchange` event and re-update the state.
+      if (deletion) {
+        // if empty value, move the cursor backwards and update the
+        // state
+        if (!state.values[action.index]) {
+          state.cursor = Math.max(0, action.index - 1);
+
+          // let know the handle-change action that we already moved
+          // backwards and that we don't need to touch the cursor
+          // anymore
+          state.backspace = true;
+
+          return { ...state };
+        }
+
+        // otherwise just return the same state and let the onchange
+        // event do the job
+      }
+
+      break;
     }
   }
 
-  return { ...state };
+  return state;
 }
 
-export const PinFieldV2: FC<Props> = props => {
-  const length = props.length || defaultProps.length;
+type Handler = {
+  refs: RefObject<HTMLInputElement[]>;
+  state: State;
+  dispatch: ActionDispatch<[Action]>;
+  value: string;
+  setValue: (value: string) => void;
+};
+
+export function usePinField(length?: number): Handler {
+  return useInternalHandler(length);
+}
+
+export function useInternalHandler(length: number = defaultProps.length, handler?: Handler): Handler {
+  if (handler) return handler;
+
   const refs = useRef<HTMLInputElement[]>([]);
   const [state, dispatch] = useReducer(reducer, defaultState(length));
 
-  console.log("state", state);
+  const value = useMemo(() => {
+    let value = "";
+    for (let index = 0; index < state.values.length; index++) {
+      value += index in state.values ? state.values[index] : "";
+    }
+    return value;
+  }, [state]);
 
-  function setRefAtIndex(idx: number) {
-    return function (ref: HTMLInputElement) {
-      if (ref) {
-        refs.current[idx] = ref;
-      }
-    };
+  const setValue = useCallback(
+    (value: string) => {
+      dispatch({ type: "handle-change", index: 0, value, reset: true });
+    },
+    [dispatch, state.cursor],
+  );
+
+  return { refs, state, dispatch, value, setValue };
+}
+
+export const PinFieldV2: FC<Props> = forwardRef((props, fwdRef) => {
+  const handler = useInternalHandler(props.length, props.handler);
+
+  useImperativeHandle(fwdRef, () => handler.refs.current, [handler.refs]);
+
+  console.log("state", handler.state);
+
+  function setRefAt(index: number): (ref: HTMLInputElement) => void {
+    return useCallback(
+      ref => {
+        if (ref) {
+          handler.refs.current[index] = ref;
+        }
+      },
+      [index],
+    );
+  }
+
+  function handleKeyDownAt(index: number): KeyboardEventHandler<HTMLInputElement> {
+    return useCallback(
+      event => {
+        handler.dispatch({ type: "handle-key-down", index, event });
+      },
+      [index, handler.dispatch],
+    );
+  }
+
+  function handleChangeAt(index: number): ChangeEventHandler<HTMLInputElement> {
+    return useCallback(
+      event => {
+        // should not happen, mostly for typescript to infer properly
+        if (!(event.nativeEvent instanceof InputEvent)) return;
+        handler.dispatch({ type: "handle-change", index, value: event.nativeEvent.data });
+      },
+      [index, handler.dispatch],
+    );
+  }
+
+  function startCompositionAt(index: number): CompositionEventHandler<HTMLInputElement> {
+    return useCallback(() => {
+      handler.dispatch({ type: "start-composition", index });
+    }, [index, handler.dispatch]);
+  }
+
+  function endCompositionAt(index: number): CompositionEventHandler<HTMLInputElement> {
+    return useCallback(
+      event => {
+        handler.dispatch({ type: "end-composition", index, value: event.data });
+      },
+      [index, handler.dispatch],
+    );
   }
 
   useEffect(() => {
-    if (!refs.current) return;
-    refs.current[state.cursor].focus();
-  }, [refs, state.cursor]);
+    if (props.onChange === undefined) return;
+    props.onChange(handler.value);
+  }, [props.onChange, handler.value]);
+
+  useEffect(() => {
+    if (!handler.refs.current) return;
+    console.log("state changed");
+
+    let innerFocus = false;
+
+    for (let index = 0; index < handler.state.values.length; index++) {
+      const value = index in handler.state.values ? handler.state.values[index] : "";
+      handler.refs.current[index].value = value;
+      innerFocus = innerFocus || hasFocus(handler.refs.current[index]);
+    }
+
+    if (innerFocus) {
+      handler.refs.current[handler.state.cursor].focus();
+    }
+  }, [handler.refs, handler.state]);
 
   return (
     <>
-      {range(0, length).map(key => (
+      {range(0, handler.state.length).map(index => (
         <input
           type="text"
           autoCapitalize="off"
           autoCorrect="off"
           autoComplete="off"
           inputMode="text"
-          key={key}
-          ref={setRefAtIndex(key)}
-          onChange={evt => dispatch({ type: "handle-change", key, value: evt.target.value })}
+          key={index}
+          ref={setRefAt(index)}
+          onKeyDown={handleKeyDownAt(index)}
+          onChange={handleChangeAt(index)}
+          onCompositionStart={startCompositionAt(index)}
+          onCompositionEnd={endCompositionAt(index)}
         />
       ))}
     </>
   );
-};
+});
+
+export function hasFocus(el: HTMLElement): boolean {
+  try {
+    const matches = el.webkitMatchesSelector || el.matches;
+    return matches.call(el, ":focus");
+  } catch (err: any) {
+    return false;
+  }
+}
 
 export default PinFieldV2;
